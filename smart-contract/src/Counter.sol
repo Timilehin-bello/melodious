855fb5e895dfa4c9969214531db7ec9cdab05e27
Lamsy <olamide.adetula.1744@gmail.com>
2024-09-02 08:28:38 +0100

    smart contract intregration

--- smart-contract/src/Counter.sol
+++ smart-contract/src/Counter.sol
@@ -1,14 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Counter {
-    uint256 public number;
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
-    }
-
-    function increment() public {
-        number++;
-    }
+ /**
+  * @title Melodious
+  * @author Lamsy and the-first-elder
+  * @notice this is the melodious smart contract
+  */
+contract Melodious {
+   
+    
 }
