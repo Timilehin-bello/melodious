924c9214dbb6c73195e25e4deffb7217d57a52b9
Lamsy <olamide.adetula.1744@gmail.com>
2024-09-13 21:38:57 +0100

    fix owner issue

--- smart-contract/src/MelodiousNFT721.sol
+++ smart-contract/src/MelodiousNFT721.sol
@@ -2,11 +2,13 @@
 pragma solidity ^0.8.22;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MelodiousNFT721 is ERC721 {
+
+contract MelodiousNFT721 is ERC721 , Ownable {
     mapping(uint256 => bytes) private songIdToUri;
 
-    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
+    constructor(string memory _name, string memory _symbol, address _owner) ERC721(_name, _symbol) Ownable(_owner ) {}
 
     function safeMint(uint256 _id, address to, bytes memory uri) public {
         songIdToUri[_id] = uri;
