57b1a74da8fc2b39d492021a653a34c8741071c4
Timilehin-bello <emmatimmybello@gmail.com>
2025-03-08 12:55:13 +0100

    delete migration files

--- server/prisma/migrations/20241007143007_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Greeting" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "message" TEXT NOT NULL,
-    "creatorAddress" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
-);
