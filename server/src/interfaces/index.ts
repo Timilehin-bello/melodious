ba4969fa67b0ddf560f08b522fb4de7130de5953
Timilehin-bello <emmatimmybello@gmail.com>
2025-03-11 20:44:05 +0100

    creeate config script and move interface types to folder

--- server/src/interfaces/index.ts
+++ server/src/interfaces/index.ts
@@ -8,3 +8,20 @@ export interface ArtistListeningStats {
   walletAddress: string;
   totalListeningTime: number;
 }
+
+export interface IMethodHandlers {
+  [key: string]: (
+    cleanedPayload: any,
+    tx: { data: string; signer: string }
+  ) => Promise<any>;
+}
+
+export interface ITransactionPayload {
+  method: string;
+  args: any;
+}
+
+export interface ITransaction {
+  data: string;
+  signer: string;
+}
